b7cc0bdf335164ef12ce329555f4565192442634
Your Name <you@example.com>
2020-09-27 18:41:51 +0200

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var correct_answer;
 var currentQuestion = -1;
+var questions;
+var token;
 
 async function getToken() {
   const tokenUrl = "https://opentdb.com/api_token.php?command=request";
@@ -9,8 +10,9 @@ async function getToken() {
   return tokenJson.token;
 }
 
-async function getQuestion() {
-  var amountOfQuestions = 0;
+async function getQuestions() {
+  var amountOfQuestions;
+  var list = [];
 
   const questionUrl = new URL("https://opentdb.com/api.php");
   questionUrl.searchParams.append("amount", "10");
@@ -24,42 +26,61 @@ async function getQuestion() {
 
   amountOfQuestions = questionJson.results.length;
 
-  var question = questionJson.results;
-
-  return [question, amountOfQuestions];
-}
-
-function setQuestion() {
-  getQuestion().then((data) => {
-    var [results, amountOfQuestions] = data;
-
-    currentQuestion++;
-    document.querySelector(".question").textContent =
-      results[currentQuestion].question;
-    correct_answer =
-      results[currentQuestion].correct_answer === "True" ? true : false;
-    //console.log(correct_answer)
-  });
-}
-function button_pressed() {
-  console.log(this.id);
-  if (correct_answer != (this.id === "true_button" ? true : false)) {
-    document.querySelector(".container").classList.add("shake_animation");
-    setTimeout(() => {document.querySelector(".container").classList.remove("shake_animation");}, 1000);
-    
-  } else {
-    document.body.style.setProperty("--border_shadow_color", "#1d7407");
-    setTimeout(() => {document.body.style.setProperty("--border_shadow_color", "violet")}, 300)
-    
+  for (i = 0; i < amountOfQuestions; i++) {
+    list.push({
+      question: questionJson.results[i].question,
+      answer: questionJson.results[i].correct_answer,
+    });
   }
-  setQuestion();
+  return list;
 }
 
-document.querySelectorAll(".answer_button").forEach((item) => {
-  item.addEventListener("click", button_pressed);
-});
-
-var token = getToken().then((data) => {
+getToken().then((data) => {
   token = data;
-  setQuestion();
+  //borde set token to lokal storage
+
+  getQuestions().then((data) => {
+    updateQuestion();
+
+    function updateQuestion() {
+      currentQuestion++
+      console.log(currentQuestion)
+
+      if (currentQuestion != (data.length)) {
+        document.querySelector(".question").innerHTML = data[currentQuestion].question;
+      }else{
+        currentQuestion = -1;
+        document.querySelector(".question").innerHTML = data[currentQuestion].question;
+      };
+      
+    }
+
+    function button_pressed() {
+      console.log(this.id);
+      console.log(data);
+
+      if (
+        data[currentQuestion].answer !=
+        (this.id === "true_button" ? "True" : "False")
+      ) {
+        document.querySelector(".container").classList.add("shake_animation");
+        setTimeout(() => {
+          document
+            .querySelector(".container")
+            .classList.remove("shake_animation");
+        }, 1000);
+      } else {
+        document.body.style.setProperty("--border_shadow_color", "#1d7407");
+        setTimeout(() => {
+          document.body.style.setProperty("--border_shadow_color", "violet");
+        }, 300);
+      }
+
+      updateQuestion();
+    }
+
+    document.querySelectorAll(".answer_button").forEach((item) => {
+      item.addEventListener("click", button_pressed);
+    });
+  });
 });
